b825d2f51b432bac90566d4c958b8a1d1618e39d
Manu Aceituno <manuel.n.aceituno@gmail.com>
2020-08-20 10:53:26 -0700

    Added client (webpack, react, babel, jest)

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => <div>Project working</div>;
+
+ReactDOM.render(<App />, document.getElementById("app"));
