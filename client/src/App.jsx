a4c52302f7b98219ec37ed459e076b123f6a8d56
Manu Aceituno <manuel.n.aceituno@gmail.com>
2020-08-20 10:58:20 -0700

    Added shards-react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
 import "./index.css";
 
 const App = () => <div>Project working</div>;
