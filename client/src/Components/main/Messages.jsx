eadadb25b8e70fca03c7fd8f841375325f6c8e29
Manu Aceituno <manuel.n.aceituno@gmail.com>
2020-08-22 12:02:25 -0700

    Autoscrolls to bottom when new message is added.

--- client/src/Components/main/Messages.jsx
+++ client/src/Components/main/Messages.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Alert } from 'shards-react';
 import { GET_MESSAGES } from '../../Queries/messages';
 import {  useSubscription } from '@apollo/client';
 
 const Messages = ({ user }) => {
     const { loading, error, data } = useSubscription(GET_MESSAGES)
-
+    const messagesEndRef = useRef(null)
+    const scrollToBottom = () => {
+      if(messagesEndRef.current) messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
+    }
+    
+    useEffect(scrollToBottom, [data]);
     if (loading) return <Alert theme='info'>Loading...</Alert>;
     if (error) return <Alert theme='danger'>Error!</Alert>;
 
@@ -13,6 +18,7 @@ const Messages = ({ user }) => {
       <div key={id} className={`message ${user === messageUser ? 'mine' : 'notMine'}`}> 
         {user !== messageUser && (<div className='userLetters'>{messageUser.slice(0,2).toUpperCase()}</div>)}
         <div className={`messageContent ${user === messageUser ? 'mine' : 'notMine'}`}>{content}</div>
+        <div ref={messagesEndRef}/>
       </div>
     )))
 };
